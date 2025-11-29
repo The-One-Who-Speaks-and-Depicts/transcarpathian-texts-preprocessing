81d191a1dd285bbd35970d1ac9bb2d33e0e377a4
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2025-11-29 21:57:57 +0100

    ADD: implement pos eval

--- src/text_level/pos.py
+++ src/text_level/pos.py
@@ -1,15 +1,20 @@
 import logging
 
 import re
+
+from sklearn.metrics import precision_recall_fscore_support
 import stanza
 from stanza.utils.conll import CoNLL
 import ntpath
 from copy import deepcopy
 import os
 
+import numpy as np
+from numpy.linalg import norm
+
 logger = logging.getLogger('preprocessor')
 
-def pos_tag_with_stanza(input_file_path: str, exp_folder: str, lang: str = 'uk'):
+def pos_tag_with_stanza(input_file_path: str, exp_folder: str, lang: str = 'uk') -> None:
     logger.debug("%s is called with arguments %s", input_file_path, locals())
     name = ntpath.basename(input_file_path)
     stanza.download(lang)
@@ -35,8 +40,205 @@ def pos_tag_with_stanza(input_file_path: str, exp_folder: str, lang: str = 'uk')
                 doc_processed.sentences[sent_idx].tokens[tkn_idx].words[wrd_idx].text = doc.sentences[sent_idx].tokens[tkn_idx].words[wrd_idx].text
     file_to_store = os.path.join(exp_folder, f"pos_stanza_{name}")
     CoNLL.write_doc2conll(doc_processed, file_to_store)
-    logger.debug("See results of morphological tagging in %s", file_to_store)
+    logger.info("See results of morphological tagging in %s", file_to_store)
     file_to_edit = os.path.join(exp_folder, f"pos_gold_{name}")
     CoNLL.write_doc2conll(doc_processed, file_to_edit)
-    logger.debug("Edit results of morphological tagging in %s", file_to_edit)
+    logger.info("Edit results of morphological tagging in %s", file_to_edit)
+
+
+def collect_labels(doc: stanza.Document) -> dict:
+    result = {'pos': [], 'feats': ['_']}    
+    for sent in doc.sentences:
+        for token in sent.tokens:
+            for word in token.words:
+                result['pos'].append(word.upos)
+                if word.feats:
+                    result['feats'].extend([i.split('=')[0] for i in word.feats.split('|') if i and i.strip()])
+    result['pos'] = list(set(result['pos']))
+    result['feats'] = list(set(result['feats']))
+    return result
+
+def score_for_single_pos(pred: list, true: list, label: str) -> None:
+    overall_size = len(true)
+    size_of_label = len([i for i in true if i == label])
+    share = size_of_label/overall_size
+    correct = len([i for i in zip(pred, true) if i[1] == label and i[0] == i[1]])
+    # I do not score F1 for single label, because for labels with low share it is going to be very low-informative
+    acc = correct/size_of_label
+    logger.info("Accuracy for %s is %.2f%s, the share of %s in dataset is %.2f%s", label, acc*100, "%", label, share*100, "%")
+
+def score_single_feat_acc(key: str, value: str, true: dict) -> int:
+    if key not in true.keys():
+        return 0
+    if true[key] != value:
+        return 0.5
+    return 1
+
+def score_feats_acc(pred: str | None, true: str | None) -> float:
+    logger.debug("Starting scoring accuracy for %s and %s", pred, true)
+    if (pred == true):
+        return 1
+    if pred == None:
+        if true == None:
+            return 1
+        return 0
+    if true == None:
+        return 0
+    pred_items = [i for i in pred.split('|') if i and i.strip()]
+    true_items = [i for i in true.split('|') if i and i.strip()]
+    pred_items_dict = dict([i.split('=') for i in pred_items])
+    true_items_dict = dict([i.split('=') for i in true_items])
+    all_items = list(pred_items_dict.keys())
+    all_items.extend(list(true_items_dict.keys()))
+    total_len = len(set(all_items))
+    logger.debug('All keys for %s and %s are %s',
+                 pred,
+                 true,
+                 all_items)
+    acc_by_feat = []
+    for k, v in pred_items_dict.items():
+        acc_by_feat.append(score_single_feat_acc(k, v, true_items_dict))
+    for k in true_items_dict.keys():
+        if k not in pred_items_dict.keys():
+            acc_by_feat.append(0)
+    logger.debug('Total keys: %s, analysed keys: %s', total_len, len(acc_by_feat))
+    assert len(acc_by_feat) == total_len
+    return sum(acc_by_feat)/total_len
+
+        
+
+
+def check_for_label_in_feats(feats: str | None, label: str) -> bool:
+    logger.debug('Checking for existence of %s in %s', label, feats)
+    if (feats == None):
+        return label == '_'
+    feats = feats.split('|')
+    for feat in feats:
+        if feat.split('=')[0] == label:
+            return True
+    return False
+
+def check_for_correct_feat(pred_feats: str | None, correct_feats: str | None, label: str) -> bool:
+    logger.debug('Evaluating %s and %s', pred_feats, correct_feats)
+    if label == '_':
+        return pred_feats == None and correct_feats == None
+    logger.debug('Label is not underscore')
+    if pred_feats == None:
+        if correct_feats == None:
+            return True
+        return False
+    if correct_feats == None:
+        return False
+    pred_feats_dict = dict([i.split('=') for i in pred_feats.split('|') if i and i.strip()])
+    correct_feats_dict = dict([i.split('=') for i in correct_feats.split('|') if i and i.strip()])
+    if label not in pred_feats_dict.keys() and label in correct_feats_dict.keys():
+        logger.debug("Incoincidence! %s and %s for label %s", pred_feats, correct_feats, label)
+        return False
+    if pred_feats_dict[label] == correct_feats_dict[label]:
+        logger.debug("Found coincidence! %s and %s for label %s", pred_feats, correct_feats, label)
+        return True
+
+
+def score_for_single_morph_feature(pred: list, true: list, label: str) -> None:
+    relevant_tokens = [i for i in zip(pred, true) if check_for_label_in_feats(i[1], label)]
+    correct = len([i for i in relevant_tokens if check_for_correct_feat(i[0], i[1], label)])
+    # I do not score F1 for single label, because for labels with low share it is going to be very low-informative
+    acc = correct/len(relevant_tokens)
+    logger.info("Accuracy for %s is %.2f%s from %s tokens", label, acc*100, "%", len(relevant_tokens))
+
+def robustness_score(original_score: float, current_score: float):
+    ideal_vector = np.array([25, 0])
+    current_vector = np.array([25, current_score - original_score])
+    cos_sim = np.dot(ideal_vector, current_vector) / (norm(ideal_vector) * norm(current_vector))
+    logger.debug('Sign-neutral cosine similarity is %.2f', cos_sim)
+    robustness_coefficient = 1 if current_score >= original_score else -1
+    return cos_sim * robustness_coefficient
+
+def score_rapidity_rate(pred: any, true: any) -> int:
+    if pred.upos != true.upos:
+        return 7
+    if pred.feats == None:
+        if true.feats == None:
+            return 0
+        return len([i for i in true.feats.split('|') if i and i.strip()])
+    if true.feats == None:
+        return len([i for i in pred.feats.split('|') if i and i.strip()])
+    gold_feats = [i for i in true.feats.split('|') if i and i.strip()]
+    pred_feats = [i for i in pred.feats.split('|') if i and i.strip()]
+    rapidity_rate = abs(len(gold_feats) - len(pred_feats))
+    gold_feats = dict([i.split('=') for i in gold_feats if i and i.strip()])
+    pred_feats = dict([i.split('=') for i in pred_feats if i and i.strip()])
+    for i in gold_feats.keys():
+        if i in pred_feats.keys():
+            if gold_feats[i] != pred_feats[i]:
+                rapidity_rate = rapidity_rate + 1
+    return rapidity_rate
+
+def assign_rapidity_rate(pred: stanza.Document, gold: stanza.Document) -> stanza.Document:
+    for sent_idx, sent in enumerate(pred.sentences):
+        for token_idx, token in enumerate(sent.tokens):
+            for word_idx, word in enumerate(token.words):
+                gold_word = gold.sentences[sent_idx].tokens[token_idx].words[word_idx]
+                rapidity_rate = score_rapidity_rate(word, gold_word)
+                rate_param = f'PosRapidity={rapidity_rate}'
+                if gold.sentences[sent_idx].tokens[token_idx].words[word_idx].misc == None:
+                    gold.sentences[sent_idx].tokens[token_idx].words[word_idx].misc = rate_param
+                else:
+                    gold.sentences[sent_idx].tokens[token_idx].words[word_idx].misc = gold.sentences[sent_idx].tokens[token_idx].words[word_idx].misc + '|' + rate_param
+    return gold
+
+
+
+def eval_pos(gold_file_path: str, pred_file_path: str, train_result: float, train_ufeats: float, exp_folder: str) -> None:
+    logger.info("POS: starting comparison between %s and %s", gold_file_path, pred_file_path)
+    gold = CoNLL.conll2doc(gold_file_path)
+    labels = collect_labels(gold)
+    logger.info("PoS and morphological labels are %s", labels)
+    pred = CoNLL.conll2doc(pred_file_path)
+    gold_pos = []
+    for sent in gold.sentences:
+        for token in sent.tokens:
+            for word in token.words:
+                gold_pos.append(word.upos)
+    pred_pos = []
+    for sent in pred.sentences:
+        for token in sent.tokens:
+            for word in token.words:
+                pred_pos.append(word.upos)
+    common_results = precision_recall_fscore_support(gold_pos, pred_pos, average='macro')
+    logger.info('PoS precision: %.2f%s', common_results[0] * 100, "%")
+    logger.info('PoS recall: %.2f%s', common_results[1] * 100, "%")
+    logger.info('PoS macro F1-score: %.2f%s', common_results[2] * 100, "%")
+    if (train_result and train_result >= 0):
+        logger.info('PoS robustness:  %.2f', robustness_score(train_result, common_results[2] * 100))
+    for pos in labels['pos']:
+        logger.debug('Starting scoring accuracy score for %s', pos)
+        score_for_single_pos(pred_pos, gold_pos, pos)
+    gold_morph = []
+    for sent in gold.sentences:
+        for token in sent.tokens:
+            for word in token.words:
+                gold_morph.append(word.feats)
+    pred_morph = []
+    for sent in pred.sentences:
+        for token in sent.tokens:
+            for word in token.words:
+                pred_morph.append(word.feats)
+    morph_comparison = [score_feats_acc(i[0], i[1]) for i in zip(pred_morph, gold_morph)]
+    morph_acc = sum(morph_comparison)/len(morph_comparison)
+    logger.info("UFeats match by keys:  %.2f%s", morph_acc*100, "%")
+    ufeats_exact = len([i for i in zip(gold_morph, pred_morph) if i[0] == i[1]])/len(gold_morph)
+    logger.info("UFeats exact match: %.2f%s", ufeats_exact*100, "%")
+    if (train_ufeats and train_ufeats >= 0):
+        logger.info('UFeats exact robustness:  %.2f', robustness_score(train_ufeats, ufeats_exact * 100))
+    for morph in labels['feats']:
+        logger.debug('Starting scoring accuracy score for %s', morph)
+        score_for_single_morph_feature(pred_morph, gold_morph, morph)
+    gold_with_rapidity = assign_rapidity_rate(pred, gold)
+    name = ntpath.basename(gold_file_path)
+    file_to_store = os.path.join(exp_folder, f"rapidity_{name}")
+    CoNLL.write_doc2conll(gold_with_rapidity, file_to_store)
+    logger.info("See results of assigned rapidity in %s", file_to_store)
+    logger.info("PoS and morphological tagging evaluation is finished")
+    
     
--- src/text_level/tagger.py
+++ src/text_level/tagger.py
@@ -3,21 +3,26 @@ import logging
 
 import yaml
 
+from datetime import datetime
+
 from corpus_distance.pipeline import create_and_set_storage_directory
 
-from pos import pos_tag_with_stanza
+from pos import pos_tag_with_stanza, eval_pos
 
 def set_logger(folder: str):
     logger = logging.getLogger('preprocessor')
-    logger.setLevel(logging.DEBUG)
-    log_handler = logging.FileHandler(filename=os.path.join(folder, 'preprocessor.log'), encoding='utf-8')
+    # TODO: logger level in config
+    logger.setLevel(logging.INFO)
+    now = datetime.now()
+    date_time = now.strftime("%m-%d-%Y_%H-%M-%S")
+    log_handler = logging.FileHandler(filename=os.path.join(folder, f'preprocessor{date_time}.log'), encoding='utf-8')
     log_formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s: %(message)s",
                                 datefmt='%Y-%m-%d %H:%M:%S')
     log_handler.setFormatter(log_formatter)
     logger.addHandler(log_handler)
     return logger
 
-def perform_initial_check_config(config: any, exp_dir: str) -> None:
+def perform_initial_check_config(config: any) -> None:
     if not bool(config):
         raise ValueError("Config is not set")
     if not bool(config["folder_name"]):
@@ -28,30 +33,46 @@ def perform_initial_check_config(config: any, exp_dir: str) -> None:
 def main():
     with open(os.path.join(os.getcwd(), 'config.yaml'), 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
+    # TODO: set separate path to dir with exps and hide it in .env, only each exp dir should be in .yaml
     exp_dir = os.path.join(os.getcwd(), config["folder_name"])
-    perform_initial_check_config(config, exp_dir)
+    perform_initial_check_config(config)
+    if not bool(config["tagging_settings"]):
+        raise ValueError("No settings for tagging")
     logger = set_logger(exp_dir)
     logger.debug('Configuration loaded: %s', config)
     if not bool(config["tagging_settings"]):
         raise ValueError("No settings for tagging")
-    settings = config["tagging_settings"]
-    if not bool(settings["input_file"]) or not os.path.exists(settings["input_file"]):
-        raise ValueError("Input file does not exist")
-    if not bool(settings["stage"]):
+    if not bool(config["phase"]):
+        raise ValueError("Phase parameter is not set")
+    phase = config["phase"]
+    if phase not in ["eval", "pred"]:
+        logger.error(
+            "Use either pred to generate file with automatic tagging" +
+            ", or eval to contrast automatic tagging with gold standard"
+            )
+        raise ValueError("Phase is set incorrectly, see log for details")    
+    settings = config["tagging_settings"] if phase == "pred" else config["evaluation_settings"]    
+    if not bool(config["stage"]):
         raise ValueError("Stage parameter is not set")
-    if not bool(settings["phase"]):
-        raise ValueError("Phase parameter is not set")    
-    match settings["stage"]:
+    match config["stage"]:
         case "pos":
-            match settings["phase"]:
+            match phase:
                 case "pred":
+                    if not bool(settings["input_file"]) or not os.path.exists(settings["input_file"]):
+                        raise ValueError("Input file does not exist")
                     pos_tag_with_stanza(settings["input_file"], exp_dir)
-                case _:
-                    logger.error(
-                        "Use either pred to generate file with automatic tagging" +
-                        ", or eval to contrast automatic tagging with gold standard"
-                        )
-                    raise ValueError("Phase is set incorrectly, see log for details")
+                case "eval":
+                    if not bool(settings["pred_file"]) or not os.path.exists(settings["pred_file"]):
+                        raise ValueError("Input file does not exist")
+                    if not bool(settings["gold_file"]) or not os.path.exists(settings["gold_file"]):
+                        raise ValueError("Input file does not exist")
+                    if not "train_result" in settings.keys() or not bool(settings["train_result"]) or settings["train_result"] == "not_set" or type(settings["train_result"]) != float or settings["train_result"] < 0:
+                        logger.warning("Train result not supplied, setting to default value")
+                        settings["train_result"] = -1
+                    if not "train_ufeats" in settings.keys() or not bool(settings["train_ufeats"]) or settings["train_ufeats"] == "not_set" or type(settings["train_ufeats"]) != float or settings["train_ufeats"] < 0:
+                        logger.warning("Train ufeats accuracy not supplied, setting to default value")
+                        settings["train_ufeats"] = -1
+                    eval_pos(settings["gold_file"], settings["pred_file"], settings["train_result"], settings["train_ufeats"], exp_dir)                    
         case _:
             logger.error("Use one of the following:" +
                          "pos for joined part-of-speech and morphological tagging," +
