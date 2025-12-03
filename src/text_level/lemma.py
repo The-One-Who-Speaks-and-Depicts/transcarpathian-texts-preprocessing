d76dc45b0a1336ee591083507563573941a2a3ad
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2025-12-03 23:00:15 +0100

    ADD: insert DeepSeek-draft of LCS finder

--- src/text_level/lemma.py
+++ src/text_level/lemma.py
@@ -55,3 +55,36 @@ def lemmatise_with_stanza(input_file_path: str, exp_folder: str, lang: str = 'uk
     file_to_edit = os.path.join(exp_folder, f"lemma_{pretagging_info}_gold_{name}")
     CoNLL.write_doc2conll(doc_processed, file_to_edit)
     logger.debug("Edit results of lemmatisation in %s", file_to_edit)
+
+
+def longest_common_substring_indices(str1, str2):
+    """Вяртае індэксы найдоўжага агульнага падрадка ў форме:
+    ((start1, end1), (start2, end2))
+    дзе end1 і end2 - індэксы ПАСЛЯ апошняга сімвала
+    """
+    m, n = len(str1), len(str2)
+    
+    # Матрыца для дынамічнага праграмавання
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    max_length = 0
+    end_pos1 = 0  # end пазіцыя ў str1 (індыкс ПАСЛЯ апошняга сімвала)
+    end_pos2 = 0  # end пазіцыя ў str2
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_pos1 = i  # i - гэта пазіцыя ў str1 (з 1)
+                    end_pos2 = j  # j - гэта пазіцыя ў str2 (з 1)
+    
+    if max_length == 0:
+        return ((-1, -1), (-1, -1))
+    
+    # Вылічым пачатковыя пазіцыі
+    start1 = end_pos1 - max_length  # індыкс першага сімвала ў str1
+    start2 = end_pos2 - max_length  # індыкс першага сімвала ў str2
+    
+    return ((start1, end_pos1), (start2, end_pos2))
